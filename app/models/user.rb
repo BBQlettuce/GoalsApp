2398e55b12c8eeac231e0445aaf09ffd82d856f3
Oscar + Gina <oscar+gina@aa.io>
2015-08-24 10:40:15 -0400

    write session token methods in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,22 @@
 class User < ActiveRecord::Base
   validates :username, :password_digest, :session_token , presence: true
   validates :password, length: { minimum: 6, allow_nil: true }
+  after_initialize :ensure_session_token
   attr_reader :password
+
+  def self.generate_session_token
+    token = SecureRandom.urlsafe_base64
+    until User.find_by(session_token: token).nil?
+      token = SecureRandom.urlsafe_base64
+    end
+    token
+  end
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    return nil if user.nil?
+    user.is_password?(password) ? user : nil
+  end
 
   def password=(password)
     self.password_digest = BCrypt::Password.create(password)
@@ -11,9 +26,14 @@ class User < ActiveRecord::Base
     BCrypt::Password.new(self.password_digest).is_password?(password)
   end
 
-  def self.find_by_credentials(username, password)
-    user = User.find_by(username: username)
-    return nil if user.nil?
-    user.is_password?(password) ? user : nil
+  def reset_session_token!
+    self.session_token = User.generate_session_token
+    self.save
+    self.session_token
   end
+
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
+
 end
